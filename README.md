20c754bc561ea490921bb357324b6aeeb01ecd0f
root <root@instance-1.asia-northeast1-b.c.shining-env-346013.internal>
2022-05-07 04:09:06 +0000

    isito service management api in golang

---
+++ README.md
@@ -0,0 +1,42 @@
+
+git clone https://github.com/istio/client-go.git
+cd client-go/cmd/example
+go get .
+go run .
+export KUBECONFIG='/root/.kube/config' && export NAMESPACE='default' && go run client.go
+
+
+How to Run
+go get .
+export KUBECONFIG='/root/.kube/config' && export NAMESPACE='istio-test' && go run .
+
+go RESTful API totorial
+https://go.dev/doc/tutorial/web-service-gin
+go compile tutorial
+https://qiita.com/uchiko/items/64fb3020dd64cf211d4e
+
+Istio Client-go -- VirtualServices Traffic Shifting Weights
+https://blog.csdn.net/qq_29778131/article/details/108198446?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-1.pc_relevant_default&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-1.pc_relevant_default&utm_relevant_index=2
+
+https://istio.io/latest/docs/tasks/traffic-management/traffic-shifting/
+测试配置 patch vs
+https://blog.csdn.net/baobaoxiannv/article/details/110732147
+https://www.codercto.com/a/31924.html
+canary description
+https://istio.io/latest/blog/2017/0.1-canary/
+
+
+curl http://localhost:3011/api/icg/weightConfig \
+    --include \
+    --header "Content-Type: application/json" \
+    --request "POST" \
+    --data '{"id": "4","title": "The Modern Sound of Betty Carter","artist": "Betty Carter","price": 49.99}'
+
+
+curl http://localhost:3011/api/icg/weightConfig \
+    --include \
+    --header "Content-Type: application/json" \
+    --request "POST" \
+    --data '{ "ns": "istio-test", "svcname": "customers", "versions": ["v1","v2"], "weights": [30, 70]}'
+
+kubectl -n istio-test get vs reviews   -o yaml
---
+++ main.go
@@ -0,0 +1,253 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+
+	versionedclient "istio.io/client-go/pkg/clientset/versioned"
+
+	networkingv1alpha3 "istio.io/api/networking/v1alpha3"
+	"istio.io/client-go/pkg/apis/networking/v1alpha3"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ac "istio.io/client-go/pkg/applyconfiguration"
+// istio.io/client-go/pkg/applyconfiguration/utils.go
+// /Users/satoshiaikawa/client-go-master/pkg/applyconfiguration/utils.go
+
+// weights represents data about a record album.
+type weights struct {
+	Ns       string   `json:"ns"`
+	Svcname  string   `json:"svcname"`
+	Versions []string `json:"versions"`
+	Weights  []int32  `json:"weights"`
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getExample(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Hello Istio Client Go")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getIstioConfig(c *gin.Context) {
+	kubeconfig := os.Getenv("KUBECONFIG") // os.GEtenv gets environment variable
+	namespace := os.Getenv("NAMESPACE")
+
+	if len(kubeconfig) == 0 || len(namespace) == 0 {
+		log.Fatalf("Environment variables KUBECONFIG and NAMESPACE need to be set")
+	}
+
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatalf("Failed to create k8s rest client: %s", err)
+	}
+
+	ic, err := versionedclient.NewForConfig(restConfig)
+	if err != nil {
+		log.Fatalf("Failed to create istio client: %s", err)
+	}
+
+	// Test VirtualServices
+	vsList, err := ic.NetworkingV1alpha3().VirtualServices(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to get VirtualService in %s namespace: %s", namespace, err)
+	}
+
+	for i := range vsList.Items {
+		vs := vsList.Items[i]
+		log.Printf("Index: %d VirtualService Hosts: %+v\n", i, vs.Spec.GetHosts())
+		log.Printf("%+v\n", vs.Spec.GetHttp()[0].GetRoute()[0].GetWeight())
+	}
+
+	// Test DestinationRules
+	drList, err := ic.NetworkingV1alpha3().DestinationRules(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to get DestinationRule in %s namespace: %s", namespace, err)
+	}
+
+	for i := range drList.Items {
+		dr := drList.Items[i]
+		log.Printf("Index: %d DestinationRule Host: %+v\n", i, dr.Spec.GetHost())
+	}
+
+	// Test Gateway
+	gwList, err := ic.NetworkingV1alpha3().Gateways(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to get Gateway in %s namespace: %s", namespace, err)
+	}
+
+	for i := range gwList.Items {
+		gw := gwList.Items[i]
+		for _, s := range gw.Spec.GetServers() {
+			log.Printf("Index: %d Gateway servers: %+v\n", i, s)
+		}
+	}
+
+	// Test ServiceEntry
+	seList, err := ic.NetworkingV1alpha3().ServiceEntries(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to get ServiceEntry in %s namespace: %s", namespace, err)
+	}
+
+	for i := range seList.Items {
+		se := seList.Items[i]
+		for _, h := range se.Spec.GetHosts() {
+			log.Printf("Index: %d ServiceEntry hosts: %+v\n", i, h)
+		}
+	}
+	//c.IndentedJSON(http.StatusOK, "Get Istio Config")
+	c.IndentedJSON(http.StatusOK, "Get Istio Config")
+}
+
+func postWeightConfig(c *gin.Context) {
+
+	var newWeights weights
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newWeights); err != nil {
+		return
+	}
+
+	log.Printf("posted weights cifig: %+v\n", newWeights)
+	log.Printf("namespace: %+v\n", newWeights.Ns)
+	log.Printf("service name: %+v\n", newWeights.Svcname)
+	log.Printf("versions: %+v\n", newWeights.Versions)
+	log.Printf("weights: %+v\n", newWeights.Weights)
+
+	namespace := newWeights.Ns
+	kubeconfig := os.Getenv("KUBECONFIG") // os.GEtenv gets environment variable
+
+	if len(kubeconfig) == 0 || len(namespace) == 0 {
+		log.Fatalf("Environment variables KUBECONFIG and NAMESPACE need to be set")
+	}
+
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatalf("Failed to create k8s rest client: %s", err)
+	}
+
+	ic, err := versionedclient.NewForConfig(restConfig)
+	if err != nil {
+		log.Fatalf("Failed to create istio client: %s", err)
+	}
+
+	// delete vs
+	ic.NetworkingV1alpha3().VirtualServices(namespace).Delete(context.TODO(), newWeights.Svcname, v1.DeleteOptions{})
+
+	var (
+		httpRouteList            []*networkingv1alpha3.HTTPRoute
+		HTTPRouteDestinationList []*networkingv1alpha3.HTTPRouteDestination
+	)
+
+	log.Printf("version: %+v, weight: %+v\n", newWeights.Versions, newWeights.Weights)
+	for i := 0; i < len(newWeights.Versions); i++ {
+		// 定义http路由
+		HTTPRouteDestination := &networkingv1alpha3.HTTPRouteDestination{
+			Destination: &networkingv1alpha3.Destination{
+				Host:   newWeights.Svcname,
+				Subset: newWeights.Versions[i],
+			},
+			// 定义权重
+			Weight: newWeights.Weights[i],
+		}
+		HTTPRouteDestinationList = append(HTTPRouteDestinationList, HTTPRouteDestination)
+	}
+	/*
+		HTTPRouteDestination1 := &networkingv1alpha3.HTTPRouteDestination{
+			Destination: &networkingv1alpha3.Destination{
+				Host:   "reviews", //newWeights.Svcname,
+				Subset: "v1",      //newWeights.Versions[i],
+			},
+			// 定义权重
+			Weight: 27, //newWeights.Weights[i],
+		}
+		HTTPRouteDestinationList = append(HTTPRouteDestinationList, HTTPRouteDestination1)
+
+		HTTPRouteDestination2 := &networkingv1alpha3.HTTPRouteDestination{
+			Destination: &networkingv1alpha3.Destination{
+				Host:   "reviews", //newWeights.Svcname,
+				Subset: "v2",      //newWeights.Versions[i],
+			},
+			// 定义权重
+			Weight: 73, //newWeights.Weights[i],
+		}
+		HTTPRouteDestinationList = append(HTTPRouteDestinationList, HTTPRouteDestination2)
+	*/
+	httpRouteSign := networkingv1alpha3.HTTPRoute{
+
+		Route: HTTPRouteDestinationList,
+	}
+	httpRouteList = append(httpRouteList, &httpRouteSign)
+	virtualService := &v1alpha3.VirtualService{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      newWeights.Svcname,
+			Namespace: namespace,
+		},
+		Spec: networkingv1alpha3.VirtualService{
+			Hosts:    []string{newWeights.Svcname}, // 定义可访问的hosts
+			Gateways: []string{"reactapp-gateway"},
+			Http:     httpRouteList, // 为hosts 绑定路由
+		},
+	}
+	// 创建VS
+	vs, err := ic.NetworkingV1alpha3().VirtualServices(namespace).Create(context.TODO(), virtualService, v1.CreateOptions{})
+	if err != nil {
+		return
+	}
+	// 打印VS
+	log.Print(vs)
+
+	/*
+		//To allocate slice for request body
+		length, err := strconv.Atoi(c.Request.Header.Get("Content-Length"))
+		if err != nil {
+			//c.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+			//Read body data to parse json
+			body := make([]byte, length)
+			length, err = c.Request.Form.Get("artist") //Body.Read(body)
+			if err != nil && err != io.EOF {
+				//c.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+
+				//parse json
+				var jsonBody map[string]interface{}
+				err = json.Unmarshal(body[:length], &jsonBody)
+				if err != nil {
+					//c.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				fmt.Printf("%v\n", jsonBody)
+				fmt.Printf("%s\n", jsonBody.artist)
+
+				//fmt.Println(b)
+				c.IndentedJSON(http.StatusCreated, jsonBody)
+	*/
+	//c.IndentedJSON(http.StatusCreated, newWeights)
+
+	c.IndentedJSON(http.StatusCreated, vs)
+}
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/api/icg/hello", getExample)
+	router.GET("/api/icg/istioConfig", getIstioConfig)
+	router.POST("/api/icg/weightConfig", postWeightConfig)
+
+	router.Run("0.0.0.0:3011")
+
+}
